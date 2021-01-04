402e93e213d8c29b36ef6d5cb14978e61f1da92b
SliverHorn <45428434+SliverHorn@users.noreply.github.com>
2021-01-04 15:10:58 +0800

    Update authorities.go

--- server/app/api/request/authorities.go
+++ server/app/api/request/authorities.go
@@ -7,7 +7,7 @@ import (
 type CreateAuthority struct {
 	AuthorityId   string `p:"authority_id" v:"required|length:1,1000#请输入角色id|角色id长度为:min到:max位"`
 	AuthorityName string `p:"authority_name" v:"required|length:1,1000#请输入角色名字|角色名字长度为:min到:max位"`
-	ParentId      string `p:"authority_id" v:"required|length:1,1000#请输入角色父id|角色父id长度为:min到:max位"`
+	ParentId      string `p:"parent_id" v:"required|length:1,1000#请输入角色父id|角色父id长度为:min到:max位"`
 }
 
 type UpdateAuthority struct {
