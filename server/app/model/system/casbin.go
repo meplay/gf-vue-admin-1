fc641a5706c911dac8cd5d41e4de7510f7f44d83
SliverHorn <sliver_horn@qq.com>
2021-02-16 13:59:51 +0800

    新增Casbin模型

---
+++ server/app/model/system/casbin.go
@@ -0,0 +1,8 @@
+package model
+
+type Casbin struct {
+	Path        string `json:"path" gorm:"column:v1"`
+	PType       string `json:"ptype" gorm:"column:p_type"`
+	Method      string `json:"method" gorm:"column:v2"`
+	AuthorityId string `json:"rolename" gorm:"column:v0"`
+}
