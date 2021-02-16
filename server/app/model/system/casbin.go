e697f7a86006b5405d815f068f828ad821692d14
SliverHorn <sliver_horn@qq.com>
2021-02-16 14:21:49 +0800

    添加Casbin模型的orm tag

--- server/app/model/system/casbin.go
+++ server/app/model/system/casbin.go
@@ -1,10 +1,10 @@
 package model
 
 type Casbin struct {
-	Path        string `json:"path" gorm:"column:v1"`
-	PType       string `json:"ptype" gorm:"column:p_type"`
-	Method      string `json:"method" gorm:"column:v2"`
-	AuthorityId string `json:"rolename" gorm:"column:v0"`
+	Path        string `orm:"v1" json:"path" gorm:"column:v1"`
+	PType       string `orm:"p_type" json:"ptype" gorm:"column:p_type"`
+	Method      string `orm:"v2" json:"method" gorm:"column:v2"`
+	AuthorityId string `orm:"v0" json:"rolename" gorm:"column:v0"`
 }
 
 func (c *Casbin) TableName() string {
