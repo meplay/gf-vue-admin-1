05ef28293f4b0b8387b57ac17618f4cbb59f1546
SliverHorn <sliver_horn@qq.com>
2021-09-02 17:16:40 +0800

    feature: boot层 redis

---
+++ boot/router.go
@@ -0,0 +1,11 @@
+package boot
+
+var Routers = new(_router)
+
+type _router struct{}
+
+// Initialize 路由初始化
+// Author [SliverHorn](https://github.com/SliverHorn)
+func (r *_router) Initialize() {
+
+}
