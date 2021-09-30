c6b0ec4d9da929224e16f0d50a9620b2f85e0046
SliverHorn <sliver_horn@qq.com>
2021-10-01 02:12:05 +0800

    feature: AuthoritiesMenus 表结构

---
+++ app/model/authorities_menus.go
@@ -0,0 +1,11 @@
+package model
+
+// AuthoritiesMenus 角色按钮关联表
+type AuthoritiesMenus struct {
+	MenuId      uint   `json:"menuId" gorm:"column:menu_id" swaggertype:"string" example:"uint 菜单ID"`
+	AuthorityId string `json:"authorityId" gorm:"column:authority_id" example:"角色ID"`
+}
+
+func (a *AuthoritiesMenus) TableName() string {
+	return "authorities_menus"
+}
