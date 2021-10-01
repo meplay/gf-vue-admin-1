6a7db94e844da42f574efed936d11c9ef6e292be
SliverHorn <sliver_horn@qq.com>
2021-10-01 21:12:12 +0800

    feature: add AuthoritiesResources 角色资源表 模型

---
+++ app/model/system/authorities_resources.go
@@ -0,0 +1,12 @@
+package system
+
+// AuthoritiesResources 角色资源表
+type AuthoritiesResources struct {
+	AuthorityId string `gorm:"column:authority_id"`
+	ResourcesId string `gorm:"column:resources_id"`
+}
+
+func (a *AuthoritiesResources) TableName() string {
+	return "authorities_resources"
+}
+
